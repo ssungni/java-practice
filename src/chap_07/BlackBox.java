cf828b6f55fd529bff8e34b4fdcd94a3394734ca
ssungni <sunny020111@gmail.com>
2024-02-26 23:05:43 +0900

    nine commit

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -6,8 +6,24 @@ public class BlackBox {
     String resolution; // 해상도
     int price; // 가격
     String color; // 색상
-
+    int serialNumber; // 시리얼 번호
+    static  int counter = 0; // 시리얼 번호를 생성해주는 역할 (처음엔 0이었다가 __ 연산을 통해 값 증가)
     static boolean conAutoReport = false; // 자동 신고 기능
+
+    BlackBox() {
+//        System.out.println("기본 생성자 호출");
+//        this.serialNumber = ++counter;
+    }
+
+    BlackBox(String modelName, String resolution, int price, String color) {
+//        this(); // 기본 생성자 호출
+//
+//        System.out.println("사용자 정의 생성자 호출");
+//        this.modelName = modelName;
+//        this.resolution = resolution;
+//        this.price = price;
+//        this.color = color;
+    }
 
     // 자동 신고 기능
     void autoReport() {
@@ -60,5 +76,46 @@ public class BlackBox {
 
     void appendModelName(String modelName) {
         this.modelName += modelName;
+    }
+
+    // Getter & Setter
+    String getModelName() {
+        return modelName;
+    }
+
+    void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    String getResolution() {
+        if (resolution == null || resolution.isEmpty()) {
+            return "판매자에게 문의하세요.";
+        }
+        return resolution;
+    }
+
+    void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    int getPrice() {
+        return price;
+    }
+
+    void setPrice(int price) {
+        if (price < 100000) {
+            this.price = 100000;
+        }
+        else {
+            this.price = price;
+        }
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    void setColor(String color) {
+        this.color = color;
     }
 }
